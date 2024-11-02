c793781dbf90936e38a7b1463c37a9fde0b1f2af
djarallah <djarallah.brahim@gmail.com>
2024-11-02 20:10:10 +0100

    init consumer project and refactor packages

---
+++ src/main/java/com/joboffer/consumer/ws/emailnotification/JobofferConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.joboffer.consumer.ws.emailnotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobofferConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobofferConsumerApplication.class, args);
+    }
+
+}
