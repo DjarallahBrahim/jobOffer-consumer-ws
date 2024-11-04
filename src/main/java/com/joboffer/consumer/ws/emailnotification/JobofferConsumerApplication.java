343efb3a0e608dd4fa2728416ba748510b7ac96a
djarallah <djarallah.brahim@gmail.com>
2024-11-04 15:48:57 +0100

    dockerise solution and update workflow

--- src/main/java/com/joboffer/consumer/ws/emailnotification/JobofferConsumerApplication.java
+++ src/main/java/com/joboffer/consumer/ws/emailnotification/JobofferConsumerApplication.java
@@ -2,9 +2,10 @@ package com.joboffer.consumer.ws.emailnotification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JobofferConsumerApplication {
+public class JobofferConsumerApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(JobofferConsumerApplication.class, args);
